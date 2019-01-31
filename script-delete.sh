4a5aa6c4f7db64bbca60a12ac63a65aa24a3fd9c
Krushon <email5000@yandex.ru>
2019-01-31 09:59:01 +0300

    Оптимизировал для ручной установки

--- script-delete.sh
+++ script-delete.sh
@@ -3,8 +3,7 @@ SECONDS=0
 printf "\033c"
 echo "Removing packages..."
 apt-get purge openvpn asterisk mc ntpdate ntp -y
-apt-get autoclean
-apt-get clean
+apt-get autoclean && apt-get clean
 rm -R /etc/openvpn
 rm /etc/iptables.rules
 apt autoremove -y

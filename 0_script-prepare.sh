3f3c34d300fd7b409aa93091b73ca08e21d4807b
Krushon <email5000@yandex.ru>
2019-02-27 14:07:55 +0300

    Переосмысление безопасности ssh-сервера

--- 0_script-prepare.sh
+++ 0_script-prepare.sh
@@ -10,13 +10,22 @@ chmod 600 /root/.ssh/authorized_keys
 # Генерация файла sshd_config для доступа по ssh-ключу
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 touch /etc/ssh/sshd_config
-echo -en "Port 22\nProtocol 2\nHostKey /etc/ssh/ssh_host_rsa_key\nHostKey /etc/ssh/ssh_host_dsa_key\n" >> /etc/ssh/sshd_config
-echo -en "HostKey /etc/ssh/ssh_host_ecdsa_key\nHostKey /etc/ssh/ssh_host_ed25519_key\nUsePrivilegeSeparation yes\n" >> /etc/ssh/sshd_config
-echo -en "KeyRegenerationInterval 3600\nServerKeyBits 1024\nSyslogFacility AUTH\nLogLevel INFO\nLoginGraceTime 120\n" >> /etc/ssh/sshd_config
-echo -en "StrictModes yes\nRSAAuthentication yes\nPubkeyAuthentication yes\nAuthorizedKeysFile      %h/.ssh/authorized_keys\n" >> /etc/ssh/sshd_config
-echo -en "RhostsRSAAuthentication no\nIgnoreRhosts yes\nHostbasedAuthentication no\nPermitEmptyPasswords no\nChallengeResponseAuthentication no\n" >> /etc/ssh/sshd_config
-echo -en "X11Forwarding yes\nX11DisplayOffset 10\nPrintMotd no\nPrintLastLog yes\nTCPKeepAlive yes\nAcceptEnv LANG LC_*\n" >> /etc/ssh/sshd_config
-echo -en "Subsystem sftp /usr/lib/openssh/sftp-server\nUsePAM yes\n" >> /etc/ssh/sshd_config
+echo -en "Port 22\nAddressFamily inet\nProtocol 2\nDebianBanner no\nPrintMotd no\n" >> /etc/ssh/sshd_config
+echo -en "SyslogFacility AUTH\nLogLevel VERBOSE\nLoginGraceTime 15\nStrictModes yes\n" >> /etc/ssh/sshd_config
+echo -en "PrintLastLog yes\nKexAlgorithms diffie-hellman-group14-sha1\n" >> /etc/ssh/sshd_config
+echo -en "HostKey /etc/ssh/ssh_host_rsa_key\nHostKey /etc/ssh/ssh_host_dsa_key\n" >> /etc/ssh/sshd_config
+echo -en "HostKey /etc/ssh/ssh_host_ecdsa_key\nHostKey /etc/ssh/ssh_host_ed25519_key\n" >> /etc/ssh/sshd_config
+echo -en "PubkeyAuthentication yes\nAuthorizedKeysFile %h/.ssh/authorized_keys\n" >> /etc/ssh/sshd_config
+echo -en "IgnoreRhosts yes\nPermitEmptyPasswords no\nHostbasedAuthentication no\n" >> /etc/ssh/sshd_config
+echo -en "ChallengeResponseAuthentication no\nKerberosAuthentication no\n" >> /etc/ssh/sshd_config
+echo -en "GSSAPIAuthentication no\nGSSAPICleanupCredentials yes\nUsePAM yes\n" >> /etc/ssh/sshd_config
+echo -en "X11DisplayOffset 10\nX11Forwarding yes\nX11UseLocalhost	no\nTCPKeepAlive yes\n" >> /etc/ssh/sshd_config
+echo -en "UsePrivilegeSeparation yes\nPermitUserEnvironment no\nClientAliveCountMax 0\n" >> /etc/ssh/sshd_config
+echo -en "UseDNS no\nMaxStartups 10:50:30\n" >> /etc/ssh/sshd_config
+echo -en "AcceptEnv LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES\n" >> /etc/ssh/sshd_config
+echo -en "AcceptEnv LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT\n" >> /etc/ssh/sshd_config
+echo -en "AcceptEnv LC_IDENTIFICATION LC_ALL LANGUAGE\nAcceptEnv XMODIFIERS\n" >> /etc/ssh/sshd_config
+echo -en "Subsystem sftp /usr/lib/openssh/sftp-server\nCompression no\nMACs hmac-sha2-256\n" >> /etc/ssh/sshd_config
 chmod 644 /etc/ssh/sshd_config
 ###
 wget https://raw.githubusercontent.com/Krushon/vds-script-vpn/master/1_script-upgrade.sh
